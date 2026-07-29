607fe1980bb8192da9dadd0b0d216cb74740232e
I_Isroilov <102636825+I-Isroilov@users.noreply.github.com>
2026-07-29 13:07:26 +0900

    Implement Batched training data (Build GPT 4/10)

---
+++ data/loader.py
@@ -0,0 +1,15 @@
+import torch
+from torchtyping import TensorType
+from typing import Tuple
+
+class Solution:
+    def create_batches(self, data: TensorType[int], context_length: int, batch_size: int) -> Tuple[TensorType[int], TensorType[int]]:
+        torch.manual_seed(0)
+        high = len(data) - context_length
+        starts = torch.randint(low=0, high=high, size=(batch_size,))
+
+        X = torch.stack([data[start: start + context_length] for start in starts])
+        Y = torch.stack([data[start + 1: start + context_length + 1] for start in starts])
+
+        return X, Y
+        
